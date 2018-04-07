01e323fb81d47c700570cb45bb48e04b4a61be2d
Austin Concepcion <snazzy2011@gmail.com>
2018-04-07 18:24:42 -0400

    v1.2 edit MainActivity to interact with Item

--- app/src/main/java/edu/valenciacollege/austinconcepcion/chapter13homework/MainActivity.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/chapter13homework/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -22,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
             SAXHandler handler = new SAXHandler();
             InputStream is = getResources().openRawResource(R.raw.test);
             saxParser.parse( is, handler);
+
+            ArrayList<Item> items = handler.getItems();
+            for (Item item : items)
+                Log.w("MainActivity", item.toString());
         }catch (Exception e){
             Log.w("MainActivity", "e= "+e.toString());
         }
