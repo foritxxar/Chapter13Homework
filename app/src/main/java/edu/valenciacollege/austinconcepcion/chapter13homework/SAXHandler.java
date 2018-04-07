ded15dbe4f2c640238f2bda693ed22bcc4dc925e
Austin Concepcion <snazzy2011@gmail.com>
2018-04-07 18:13:43 -0400

    v1.1 edit SAXHandler to interact with Item

--- app/src/main/java/edu/valenciacollege/austinconcepcion/chapter13homework/SAXHandler.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/chapter13homework/SAXHandler.java
@@ -5,24 +5,47 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.ArrayList;
+
 /**
  * Created by Austin Concepcion on 4/7/2018.
  */
 
 public class SAXHandler extends DefaultHandler {
-    public SAXHandler(){}
+    private boolean validText;
+    private String element="";
+    private Item currentItem;
+    private ArrayList<Item> items;
+
+    public SAXHandler(){
+        validText = false;
+        items = new ArrayList<Item>();
+    }
+
+    public ArrayList<Item> getItems(){return items;}
 
     public void startElement (String uri, String localName, String startElement,
                               Attributes attributes) throws SAXException{
-        Log.w("MainActivity", "Inside startElement, startElement =" +startElement);
+        validText = true;
+        element = startElement;
+        if(startElement.equals("item"))
+            currentItem = new Item("", "");
+        //Log.w("MainActivity", "Inside startElement, startElement =" +startElement);
     }
 
     public void endElement(String uri, String localName, String endElement) throws SAXException{
-        Log.w("MainActivity", "Inside endElement, endElement ="+endElement);
+        validText = false;
+        if(endElement.equals("item"))
+            items.add(currentItem);
+        //Log.w("MainActivity", "Inside endElement, endElement ="+endElement);
     }
 
     public void characters(char [] ch, int start, int length) throws SAXException{
-        String text = new String(ch, start, length);
-        Log.w("Main Activity", "Inside characters, text = "+text);
+        if(currentItem != null && element.equals("title") && validText)
+            currentItem.setTitle(new String(ch, start, length));
+        else if(currentItem!=null && element.equals("link") && validText)
+            currentItem.setLink(new String(ch, start, length));
+        //String text = new String(ch, start, length);
+        //Log.w("Main Activity", "Inside characters, text = "+text);
     }
 }
